6e19daf31adc24acd2a647d9749c9970fdcbb4fb
shawn <dhryxy@qq.com>
2020-05-22 11:51:17 +0800

    fix keyStore import

--- lib/common/consts/settings.dart
+++ lib/common/consts/settings.dart
@@ -59,4 +59,4 @@ const String acala_stable_coin = 'AUSD';
 const String acala_stable_coin_view = 'aUSD';
 
 /// test app versions
-const String app_beta_version = '0.7.5-beta.4';
+const String app_beta_version = '0.7.5-beta.6';
