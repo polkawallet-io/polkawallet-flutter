eb77d293720669a843808f6ce85a64b0afcb7efb
shawn <dhryxy@gmail.com>
2020-07-03 15:53:49 +0800

    update appcenter config and use flutter stable

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -9,14 +9,19 @@ set -x
 cd ..
 # choose a different release channel if you want - https://github.com/flutter/flutter/wiki/Flutter-build-release-channels
 # stable - recommended for production
-
 git clone -b stable https://github.com/flutter/flutter.git
 export PATH=`pwd`/flutter/bin:$PATH
-
 flutter channel stable
 flutter doctor
-cd ./lib/js_service_kusama && npm install && npm run build && cd ../..
-cd ./lib/js_service_acala && npm install && npm run build && cd ../..
+
+# update node.js version and build main.js before flutter build
+brew uninstall node@6
+NODE_VERSION="12.16.0"
+curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
+sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
+cd ./lib/js_service_kusama && yarn install && yarn run build && cd ../..
+cd ./lib/js_service_acala && yarn install && yarn run build && cd ../..
+
 flutter build apk --release
 
 # copy the APK where AppCenter will find it
