b1c058d40c0e919242567a8ffa3cf76ff732c25f
shawn <dhryxy@gmail.com>
2020-07-10 10:53:22 +0800

    update appcenter script for ios

--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -15,7 +15,7 @@ flutter channel stable
 flutter doctor
 
 # update node.js version and build main.js before flutter build
-brew uninstall node@6
+brew uninstall node
 NODE_VERSION="12.16.0"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
