c81366f7f715049aebf676844d594a3f99159c40
Christian Langenbacher <christian.langenbacher91@gmail.com>
2020-09-30 12:53:24 +0200

    remove chinese localization and unused localization categories

--- lib/utils/i18n/encointer.dart
+++ lib/utils/i18n/encointer.dart
@@ -6,8 +6,7 @@ const Map<String, String> enNctr = {
   'faucet.ok': 'Test Tokens were sent.',
   'faucet.limit': 'You have enough funds, you don\'t need the faucet',
   'faucet.error': 'Request error, try again later.',
-  'faucet.insufficientBalance':
-      'Faucet has run out of funds. Please notify the administrator',
+  'faucet.insufficientBalance': 'Faucet has run out of funds. Please notify the administrator',
   'encointer.registering': 'Registering',
   'loan.txs': 'History',
   'ceremony': 'Encointer Ceremony',
@@ -17,18 +16,4 @@ const Map<String, String> enNctr = {
   'attestation.perform': 'attest',
   'attestation.revert': 'revert',
   'meetup.complete': 'complete meetup',
-};
-
-const Map<String, String> zhNctr = {
-  'encointer': 'Encointer 稳定币平台',
-  'txs.action': '操作类型',
-  'faucet.title': '水龙头',
-  'faucet.brief': '领取 Acala 测试网使用的测试 Token.',
-  'faucet.ok': '领取成功。',
-  'faucet.limit': '每天领取次数有限哦。',
-  'faucet.error': '可能领取姿势不太对，等会儿再试试吧。',
-  'encointer.registering': 'Registering',
-  'loan.txs': 'History',
-  'ceremony': 'Encointer Ceremony',
-  'claim.qr': 'Claim'
 };
