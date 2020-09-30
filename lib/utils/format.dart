22b1e69fde4c92ad9e6bf6273265325d79e451c6
Christian Langenbacher <christian.langenbacher91@gmail.com>
2020-09-30 12:56:13 +0200

    remove validator related stuff

--- lib/utils/format.dart
+++ lib/utils/format.dart
@@ -8,10 +8,8 @@ import 'package:base58check/base58check.dart';
 import 'package:convert/convert.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
-import 'package:polka_wallet/common/consts/settings.dart';
 import 'package:polka_wallet/store/account/types/accountData.dart';
 import 'package:polka_wallet/store/app.dart';
-import 'package:polka_wallet/store/staking/types/validatorData.dart';
 import 'package:polka_wallet/utils/i18n/index.dart';
 
 class Fmt {
@@ -77,8 +75,7 @@ class Fmt {
       return '~';
     }
     value.toStringAsFixed(3);
-    NumberFormat f =
-        NumberFormat(",##0${length > 0 ? '.' : ''}${'#' * length}", "en_US");
+    NumberFormat f = NumberFormat(",##0${length > 0 ? '.' : ''}${'#' * length}", "en_US");
     return f.format(value);
   }
 
@@ -92,8 +89,7 @@ class Fmt {
     if (raw == null || raw.length == 0) {
       return '~';
     }
-    return doubleFormat(bigIntToDouble(balanceInt(raw), decimals),
-        length: length);
+    return doubleFormat(bigIntToDouble(balanceInt(raw), decimals), length: length);
   }
 
   /// combined number transform 1-2:
@@ -147,12 +143,8 @@ class Fmt {
     }
     final int x = pow(10, lengthMax ?? lengthFixed);
     final double price = (value * x).ceilToDouble() / x;
-    final String tailDecimals =
-        lengthMax == null ? '' : "#" * (lengthMax - lengthFixed);
-    return NumberFormat(
-            ",##0${lengthFixed > 0 ? '.' : ''}${"0" * lengthFixed}$tailDecimals",
-            "en_US")
-        .format(price);
+    final String tailDecimals = lengthMax == null ? '' : "#" * (lengthMax - lengthFixed);
+    return NumberFormat(",##0${lengthFixed > 0 ? '.' : ''}${"0" * lengthFixed}$tailDecimals", "en_US").format(price);
   }
 
   /// number transform 6:
@@ -168,12 +160,8 @@ class Fmt {
     }
     final int x = pow(10, lengthMax ?? lengthFixed);
     final double price = (value * x).floorToDouble() / x;
-    final String tailDecimals =
-        lengthMax == null ? '' : "#" * (lengthMax - lengthFixed);
-    return NumberFormat(
-            ",##0${lengthFixed > 0 ? '.' : ''}${"0" * lengthFixed}$tailDecimals",
-            "en_US")
-        .format(price);
+    final String tailDecimals = lengthMax == null ? '' : "#" * (lengthMax - lengthFixed);
+    return NumberFormat(",##0${lengthFixed > 0 ? '.' : ''}${"0" * lengthFixed}$tailDecimals", "en_US").format(price);
   }
 
   /// number transform 7:
@@ -192,8 +180,7 @@ class Fmt {
     if (value == null) {
       return '~';
     }
-    return priceCeil(Fmt.bigIntToDouble(value, decimals),
-        lengthFixed: lengthFixed, lengthMax: lengthMax);
+    return priceCeil(Fmt.bigIntToDouble(value, decimals), lengthFixed: lengthFixed, lengthMax: lengthMax);
   }
 
   static String priceFloorBigInt(
@@ -205,8 +192,7 @@ class Fmt {
     if (value == null) {
       return '~';
     }
-    return priceFloor(Fmt.bigIntToDouble(value, decimals),
-        lengthFixed: lengthFixed, lengthMax: lengthMax);
+    return priceFloor(Fmt.bigIntToDouble(value, decimals), lengthFixed: lengthFixed, lengthMax: lengthMax);
   }
 
   static bool isAddress(String txt) {
@@ -224,53 +210,7 @@ class Fmt {
     return reg.hasMatch(pass);
   }
 
-  static int sortValidatorList(
-      Map addressIndexMap, ValidatorData a, ValidatorData b, int sortType) {
-    if (a.commission == null || a.commission.isEmpty) {
-      return 1;
-    }
-    if (b.commission == null || b.commission.isEmpty) {
-      return -1;
-    }
-    double comA = double.parse(a.commission.split('%')[0]);
-    double comB = double.parse(b.commission.split('%')[0]);
-    var cmpStake = a.total < b.total ? 1 : -1;
-    switch (sortType) {
-      case 0:
-        return a.total != b.total ? cmpStake : comA > comB ? 1 : -1;
-      case 1:
-        return a.points == b.points ? cmpStake : a.points < b.points ? 1 : -1;
-      case 2:
-        return comA == comB ? cmpStake : comA > comB ? 1 : -1;
-      case 3:
-        final infoA = addressIndexMap[a.accountId];
-        if (infoA != null && infoA['identity'] != null) {
-          final List judgements = infoA['identity']['judgements'];
-          if (judgements != null && judgements.length > 0) {
-            return -1;
-          }
-        }
-        return 1;
-      default:
-        return -1;
-    }
-  }
-
-  static List<ValidatorData> filterValidatorList(
-      List<ValidatorData> ls, String filter, Map accIndexMap) {
-    ls.retainWhere((i) {
-      final Map accInfo = accIndexMap[i.accountId];
-      final value = filter.trim().toLowerCase();
-      return Fmt.accountDisplayNameString(i.accountId, accInfo)
-              .toLowerCase()
-              .contains(value) ||
-          i.accountId.toLowerCase().contains(value);
-    });
-    return ls;
-  }
-
-  static List<List> filterCandidateList(
-      List<List> ls, String filter, Map accIndexMap) {
+  static List<List> filterCandidateList(List<List> ls, String filter, Map accIndexMap) {
     ls.retainWhere((i) {
       String value = filter.trim().toLowerCase();
       String accName = '';
@@ -278,50 +218,9 @@ class Fmt {
       if (accInfo != null) {
         accName = accInfo['identity']['display'] ?? '';
       }
-      return i[0].toLowerCase().contains(value) ||
-          accName.toLowerCase().contains(value);
+      return i[0].toLowerCase().contains(value) || accName.toLowerCase().contains(value);
     });
     return ls;
-  }
-
-  static Map formatRewardsChartData(Map chartData) {
-    List<List> formatChart(String chartName, Map data) {
-      List<List> values = [];
-      List<String> labels = [];
-      List chartValues = data[chartName]['chart'];
-
-      chartValues.asMap().forEach((index, ls) {
-        if (index == chartValues.length - 1) {
-          List average = [];
-          List.of(ls).asMap().forEach((i, v) {
-            num avg = v - chartValues[chartValues.length - 2][i];
-            average.add(avg);
-          });
-          values.add(average);
-        } else {
-          values.add(ls);
-        }
-      });
-
-      List<String>.from(data[chartName]['labels']).asMap().forEach((k, v) {
-        if ((k - 2) % 10 == 0) {
-          labels.add(v);
-        } else {
-          labels.add('');
-        }
-      });
-      return [values, labels];
-    }
-
-    List<List> rewards = formatChart('rewards', chartData);
-    List<List> points = formatChart('points', chartData);
-    List<List> stakes = formatChart('stakes', chartData);
-
-    return {
-      'rewards': rewards,
-      'stakes': stakes,
-      'points': points,
-    };
   }
 
   static String blockToTime(int blocks, int blockDuration) {
@@ -405,9 +304,6 @@ class Fmt {
   }
 
   static String addressOfAccount(AccountData acc, AppStore store) {
-    return store.account.pubKeyAddressMap[store.settings.endpoint.ss58]
-            [acc.pubKey] ??
-        acc.address ??
-        '';
+    return store.account.pubKeyAddressMap[store.settings.endpoint.ss58][acc.pubKey] ?? acc.address ?? '';
   }
 }
